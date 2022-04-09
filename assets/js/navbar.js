aabc84f0ef0f3dfe09556d9595e8c4076c8e67bb
legenduzair <uzairoomer@live.com>
2022-04-09 16:55:16 +0000

    Add comments to describe js code in navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,9 @@
+// To acquire hamburger button and navbar links class name.
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+/* Adds click event listener for when hamburger button is clicked.
+If clicked, the navbar links are displayed. */ 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
