b9d965782a8e19a123187f035bbf6493bfd160a4
legenduzair <uzairoomer@live.com>
2022-04-09 14:12:02 +0000

    Add js code for adding books to featured and booklist sections in an if-else statement

--- assets/js/booklist.js
+++ assets/js/booklist.js
@@ -1,29 +1,29 @@
  function handleResponse(obj) {
-    obj.items.forEach((item, index) => {
-    if(index > 7) return;
+   obj.items.forEach((item, index) => {
+    let text = (item.volumeInfo.description).substring(0, 140);
+    let subtitle = item.volumeInfo.subtitle?" - " + item.volumeInfo.subtitle:"";
+    if(index <= 7) {
      let div = document.createElement('div');
      div.className = 'b-card';
      div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
-     <h1>${item.volumeInfo.title} - ${item.volumeInfo.subtitle}</h1>
+     <h1>${item.volumeInfo.title}${subtitle}</h1>
      <p><strong>${item.volumeInfo.authors[0]}</strong></p>
      <strong>Pages: ${item.volumeInfo.pageCount}</strong>
-     <p class="book-description">${item.volumeInfo.description}</p>`
+     <p class="book-description">${text}</p>`
      
      let container = document.querySelector('.booklist-cards');
      container.append(div);
-
-     obj.items.forEach((item, index) => {
-      if(index < 8) return;
-       let div = document.createElement('div');
-       div.className = 'f-card';
-       div.innerHTML = `<h1>${item.volumeInfo.title} - ${item.volumeInfo.subtitle}</h1>
-       <p><strong>${item.volumeInfo.authors[0]}</strong></p>
-       <strong>Pages: ${item.volumeInfo.pageCount}</strong>
-       <p class="book-description">${item.volumeInfo.description}</p>
-       <img src="${item.volumeInfo.imageLinks.thumbnail}" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />`
-       
-       let container = document.querySelector('.featured-cards');
-       container.append(div);
-     })
+    }  else {
+      let div = document.createElement('div');
+      div.className = 'f-card';
+      div.innerHTML = `<h1>${item.volumeInfo.title}${subtitle}</h1>
+      <p><strong>${item.volumeInfo.authors[0]}</strong></p>
+      <strong>Pages: ${item.volumeInfo.pageCount}</strong>
+      <p class="book-description">${text}</p>
+      <img src="${item.volumeInfo.imageLinks.thumbnail}" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />`
+      
+      let container = document.querySelector('.featured-cards');
+      container.append(div);
+    }
    })
  }
