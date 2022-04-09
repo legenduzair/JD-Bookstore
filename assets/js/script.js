c8c876a2b5e334257b406487a21a27788feb8c3d
legenduzair <uzairoomer@live.com>
2022-04-09 11:09:36 +0000

    Edit script.js tag to display more html elements, and edit those elements in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
-function handleResponse(obj) {
+ function handleResponse(obj) {
     obj.items.forEach((item, index) => {
      let div = document.createElement('div');
      div.className = 'b-card';
-     div.innerHTML = `<h1>${item.volumeInfo.title}</h1>
+     div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
+     <h1>${item.volumeInfo.title} - ${item.volumeInfo.subtitle}</h1>
      <p><strong>${item.volumeInfo.authors[0]}</strong></p>
-     <img src="${item.volumeInfo.imageLinks.thumbnail}" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />`
+     <strong>Pages: ${item.volumeInfo.pageCount}</strong>
+     <p class="book-description">${item.volumeInfo.description}</p>`
      
      let container = document.querySelector('.booklist-cards');
      container.append(div);
