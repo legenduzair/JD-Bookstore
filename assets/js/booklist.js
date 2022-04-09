c6afe827be2d3af141a1e0a0d41f106ff77664ca
legenduzair <uzairoomer@live.com>
2022-04-09 12:23:16 +0000

    Add js code to add books from JSON array to featured section on HTML and style featured section using CSS

---
+++ assets/js/booklist.js
@@ -0,0 +1,29 @@
+ function handleResponse(obj) {
+    obj.items.forEach((item, index) => {
+    if(index > 7) return;
+     let div = document.createElement('div');
+     div.className = 'b-card';
+     div.innerHTML = `<img src="${item.volumeInfo.imageLinks.thumbnail}" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />
+     <h1>${item.volumeInfo.title} - ${item.volumeInfo.subtitle}</h1>
+     <p><strong>${item.volumeInfo.authors[0]}</strong></p>
+     <strong>Pages: ${item.volumeInfo.pageCount}</strong>
+     <p class="book-description">${item.volumeInfo.description}</p>`
+     
+     let container = document.querySelector('.booklist-cards');
+     container.append(div);
+
+     obj.items.forEach((item, index) => {
+      if(index < 8) return;
+       let div = document.createElement('div');
+       div.className = 'f-card';
+       div.innerHTML = `<h1>${item.volumeInfo.title} - ${item.volumeInfo.subtitle}</h1>
+       <p><strong>${item.volumeInfo.authors[0]}</strong></p>
+       <strong>Pages: ${item.volumeInfo.pageCount}</strong>
+       <p class="book-description">${item.volumeInfo.description}</p>
+       <img src="${item.volumeInfo.imageLinks.thumbnail}" alt="${item.singleTitle} by ${item.volumeInfo.authors[0]}" />`
+       
+       let container = document.querySelector('.featured-cards');
+       container.append(div);
+     })
+   })
+ }
